37015280baa8f3eb9a13d788e62a0bd0270a8f56
Puthearath Aim <puthearathaim@protonmail.com>
2025-04-08 16:09:01 +0700

    minimal viable website

--- api/weather.js
+++ api/weather.js
@@ -1,22 +1,23 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const { city } = req.query;
-  const apiKey = process.env.OPENWEATHERMAP_APIKEY; 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?` + `q=${city}` + `&appid=${apiKey}` + `&units=metric`;
+    const { 'weather-type': weatherType , 
+                'location':    location } = req.query;
+    const apiKey = process.env.OPENWEATHERMAP_APIKEY; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${weatherType}?q=${location}&units=metric&appid=${apiKey}`;
 
-//   res.setHeader('Access-Control-Allow-Origin', 'https://puth2314.github.io');  
-  res.setHeader('Access-Control-Allow-Origin', '*');  
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
+    res.setHeader('Access-Control-Allow-Origin', 'https://puth2314.github.io'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
 
-  try {
-    const response = await axios.get(apiUrl);
-    res.status(200).json(response.data);
-  } catch (error) {
-    if (error.response && error.response.status === 404) {
-      return res.status(404).json(error.response.data);
+    try {
+      const response = await axios.get(apiUrl);
+      res.status(200).json(response.data);
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+          res.status(404).json({ error: `Location "${location}" not found.` });
+        } else {
+          res.status(500).json({ error: `Unexpected error occurred while fetching ${weatherType} data.` });
+        }
     }
-    res.status(500).json({ error: 'Error fetching weather data!' });
-  }
 };
---
+++ scripts/main.js
@@ -0,0 +1,144 @@
+const mainContainer = document.querySelector('main');
+const locationForm  = document.querySelector('form');
+const locationInput = document.querySelector('input');
+const resultsContainer  = document.getElementById('results-container');
+const locationContainer = document.getElementById('location-container');
+const weatherContainer  = document.getElementById('weather-container');
+const detailsContainer  = document.getElementById('details-container');
+const forecastContainer = document.getElementById('forecast-container');
+const errorContainer    = document.getElementById('error-container');
+
+locationForm.addEventListener('submit', function (submitEvent) {
+    submitEvent.preventDefault();
+    getWeather(locationInput.value, 'weather');
+});
+
+const weatherApiUrl = 'https://weather-website-ten-zeta.vercel.app/api/weather'
+
+function getWeather(location, weatherType) {
+    const weatherApiQuery = `?location=${location}&weather-type=${weatherType}`;
+    fetch(weatherApiUrl + weatherApiQuery)
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+
+            const errorCode = data.cod.toString();
+            if (errorCode !== '200') {
+              const error = new Error(data.message);
+              error.code = errorCode;
+              throw error;
+            }
+
+            if (weatherType === 'weather') {
+                displayWeather(data);
+                getWeather(location, 'forecast');
+            } else if (weatherType === 'forecast') {
+                displayForecast(data.list);
+            } else {
+              throw new Error(`Invalid weatherType, must be either 'weather' or 'forecast'.`)
+            }
+
+            showContainer(  errorContainer, false);
+            showContainer(resultsContainer,  true);
+            // mainContainer.style.height = ;
+
+        })
+        .catch(err => {
+
+            mainContainer.classList.add('shaking-anim');
+            setTimeout(() => {
+                mainContainer.classList.remove('shaking-anim');
+            }, 1000);
+
+            let errorMessage, errorAlert;
+            if (err.code === '404') {
+                errorAlert   = `location not found.`;
+                errorMessage = `Are you sure "${locationInput.value}" is a valid location?`;
+            } else {
+                errorAlert   = `unexpected error while fetching ${weatherType} data.`;
+                errorMessage = `${err.code}: ${err.message}`;
+            }
+            errorContainer.querySelector('figcaption').innerText = errorMessage;
+            // alert('Oops, ' + errorAlert);
+
+            showContainer(resultsContainer, false);
+            showContainer(  errorContainer,  true);
+            // mainContainer.style.height = ;
+
+        })
+        .finally(() => {  
+            locationInput.value = '';
+        });
+}
+
+function displayWeather(weatherData) {
+    const locationName        = weatherData.name;    
+    const locationCountryCode = weatherData.sys.country;
+    const weatherIconCode     = weatherData.weather[0].icon;
+    const weatherTemperature  = parseInt(weatherData.main.temp);
+    const weatherDescription  = weatherData.weather[0].description;
+    const detailsClouds       = weatherData.clouds.all;
+    const detailsWindSpeed    = parseInt(weatherData.wind.speed);
+
+    locationContainer.querySelector('span').innerText       = locationName;
+    locationContainer.querySelector('img').src              = `https://flagsapi.com/${locationCountryCode}/flat/32.png`; 
+    weatherContainer.querySelector('p').innerText           = `${weatherTemperature}°C`;
+    weatherContainer.querySelector('img').src               = `https://openweathermap.org/img/wn/${weatherIconCode}@4x.png`;
+    weatherContainer.querySelector('figcaption').innerText  = capitalizeWords(weatherDescription);
+    detailsContainer.querySelector('#clouds').innerText     = `${detailsClouds}%`;
+    detailsContainer.querySelector('#wind-speed').innerText = `${detailsWindSpeed}km/h`;
+} 
+
+function displayForecast(forecastData) {
+    forecastContainer.innerHTML = '';
+
+    forecastData = forecastData.slice(0, 8);
+    forecastData.forEach(item => {
+        const forecastDateTime    = new Date(item.dt * 1000);
+        const forecastHour        = forecastDateTime.getHours();
+        const forecastIconCode    = item.weather[0].icon; 
+        const forecastTemperature = parseInt(item.main.temp);
+
+        const forecastFigure  = document.createElement('figure');
+        const forecastCaption = document.createElement('figcaption');
+        const forecastImage   = document.createElement('img');
+        forecastCaption.textContent = `${forecastHour}:00`;
+        forecastImage.src = `https://openweathermap.org/img/wn/${forecastIconCode}.png`;
+        forecastFigure.appendChild(forecastCaption);
+        forecastFigure.appendChild(forecastImage);
+
+        const forecastTempSpan = document.createElement('span');
+        forecastTempSpan.textContent = `${forecastTemperature}°C`;  
+
+        const forecastItem = document.createElement('div');
+        forecastItem.classList.add('forecast-item');
+        forecastItem.appendChild(forecastFigure);
+        forecastItem.appendChild(forecastTempSpan);
+
+        forecastContainer.appendChild(forecastItem)
+    });
+}
+
+function capitalizeWords(str) {
+    return str
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) 
+        .join(' '); 
+}
+
+function showContainer(container, boolToShow) {
+    if (boolToShow) {
+        container.classList.remove('hidden');
+        container.classList.add('fade-anim');
+    } else {
+        container.classList.add('hidden');
+        container.classList.remove('fade-anim');
+    }
+}
+
+function initWebsite() {
+  getWeather('Sydney', 'weather');
+}
+
+// initWebsite();
